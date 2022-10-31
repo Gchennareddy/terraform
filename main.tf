8e666ff465398ef6567438a4c1825f6b7b0a315f
Chenna <guvvalachennareddy@gmail.com>
2022-10-31 15:51:59 +0530

    added variable for cidr block

--- main.tf
+++ main.tf
@@ -8,10 +8,15 @@ variable "region" {
   description = "region in which vpctier has to be created"
 }
 
+variable "vpccidr" {
+  type = string
+  default = "192.168.0.0/16"
+}
+
 
 # we need to create a vpc resource
 resource "aws_vpc" "vpctier" {
-    cidr_block = "192.168.0.0/16"
+    cidr_block = var.vpccidr
     enable_dns_support = true
     enable_dns_hostnames = true
     
