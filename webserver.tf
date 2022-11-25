d7ac6316899ee2d4d15b618335051f7a4919cd1b
Chenna <guvvalachennareddy@gmail.com>
2022-11-25 17:18:40 +0530

    Added appserver instance

--- webserver.tf
+++ webserver.tf
@@ -1,3 +1,17 @@
+resource "aws_instance" "appserver1" {
+    ami = data.aws_ami.ubuntu.id
+    associate_public_ip_address = false
+    instance_type = var.webserverinstancetype
+    key_name = "terraformkey"
+    security_groups = [ aws_security_group.appsg.id ]
+    subnet_id = aws_subnet.subnets[2].id
+    tags = {
+      "Name" = "appserver 1"
+    }
+    depends_on = [ aws_db_instance.vpctierdb ]
+}
+
+
 resource "aws_instance" "webserver1" {
     ami = data.aws_ami.ubuntu.id
     associate_public_ip_address = true
